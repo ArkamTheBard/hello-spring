aafe144d0c78fcb91a994128326ff3df70b1f026
Yardley Gutierrez <9100062+ArkamTheBard@users.noreply.github.com>
2021-02-14 20:03:02 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,83 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+
+    // Handles request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+
+    // Handles request of the form /hello?name=LaunchCode
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("helloLang") //this is createMessage lol
+    public String helloWithSelect(@RequestParam String name, @RequestParam String langs){
+        if(langs.equals("fr")){
+            return "Bonjour, " + name + '!';
+        }else if(langs.equals("ger")){
+            return "Hallo, " + name + '!';
+        }else if(langs.equals("sp")){
+            return "Hola, " + name + '!';
+        }else if(langs.equals("jp")){
+            return "こんにちは、 " + name  + '!';
+        }else{
+            return "Hello, " + name + '!';
+        }
+    }
+
+    //Handles request of the form /hello/LaunchCode
+
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + //submit request to /hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @GetMapping("select")
+    public String helloSelect(){
+        return "<html>" +
+                "<body>" +
+                "<form action='helloLang'>" +
+                "<input type='text' name='name'>" +
+                "<select name='langs'>" +
+                "<option value='eng'>English</option>" +
+                "<option value='fr'>French</option>" +
+                "<option value='ger'>German</option>" +
+                "<option value='sp'>Spanish</option>" +
+                "<option value='jp'>Japanese</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
